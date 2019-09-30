1f49990a720e68c6aec0430c763815c82c7c3a4a
saphidev <saphidev@gmail.com>
2019-09-30 23:35:06 +0100

    search for IoT

--- src/index.js
+++ src/index.js
@@ -4,52 +4,58 @@ var http = require('http');
 
 
 
+
+//search for the word ??
 const search = "IoT"
+
+
+
 
 http.createServer(async (req, res) => {
 
-
-    console.log("start new scraper")
-    let array = await scraper({
-        max: 2,
+    let config = {
+        max: 10,
         search
-    });
+    }
+    console.log("start new scraper",config)
 
+    //scrape array of articles
+    let articles = await scraper(config);
 
-    if (array.length == 0) {
-
+    if (articles.length == 0) {
         res.write(`no ${search} word!`); //write a response to the client
         res.end(); //end the response
-
     } else {
-
         
-        let article = array[0]
-
-        console.log(article.publicUrl)
+        let article = articles[0]
+        console.log('frist page url',article.publicUrl)
 
             let html = article.content.replace(new RegExp(`(${search})`, "g"), "<span style='background-color:yellow'>$1</span>")
-           
             res.write(`
-            <html>
+            <html lang="en-US" prefix="og: http://ogp.me/ns#">
                 <head>
+                    <meta charset="UTF-8">
                     <title>${article.title}</title>
                 </head>
                 <body>
+                    
+                    <article>
+                    <h3>${article.title}</h3>
                     ${html}
+                    </article>
+
+                    <style>
+                    article{
+                        max-width:920px;
+                        margin:0 auto;
+                        padding:4px;
+                    }
+                    </style>
                 </body>
             </html>
             `);
-        
-
-        
         res.end(); //end the response
     }
-
-
-
-
-
 
 }).listen(8080); //the server object listens on port 8080
 
